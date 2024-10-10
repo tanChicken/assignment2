38e6433a9fa343a554ed6033053404c9352dca1d
jtan0417 <jujutan6969@gmail.com>
2024-10-10 10:47:31 +0800

    removed # for testing

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,9 @@
 var vg_1 = "bar_chart.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+vegaEmbed("bar_chart", vg_1).then(function(result) {
     // Access the Vega view instance as result.view if needed(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "map.vg.json";
-vegaEmbed("#map", vg_2).then(function(result) {
+vegaEmbed("map", vg_2).then(function(result) {
     // Access the Vega view instance as result.view if needed(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
